48e9e905e7df2229ccfa93cf7eb4fedc84e39e55
Ivan Osipov <ivan.osipov.95@gmail.com>
2019-01-14 13:12:06 +0300

    added typescript support for server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/test', (req, res) => res.json({text: 'test endpoint'}));
+app.get('/api/test', (req: any, res: any) => res.json({text: 'test endpoint'}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
