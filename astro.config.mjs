5cbaa78ce7247d160aea3a9de2ba7480ef2a1fe4
Ivan Osipov <2895.olvan@gmail.com>
2026-03-24 19:14:23 +0300

    connect google and yandex update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://ivanosipov.dev',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap({ lastmod: new Date() })],
   markdown: {
     shikiConfig: {
       theme: 'vitesse-dark',
